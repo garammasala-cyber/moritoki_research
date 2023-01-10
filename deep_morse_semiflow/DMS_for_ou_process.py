6c80b459dbe3025250e5fbad098276f5f16aae60
garammasala-cyber <moritokikoji567@gmail.com>
2023-01-10 16:06:45 +0900

    同一device上で計算が行われるように修正

--- deep_morse_semiflow/DMS_for_ou_process.py
+++ deep_morse_semiflow/DMS_for_ou_process.py
@@ -81,17 +81,18 @@ class CustomLoss(nn.Module):
 
     def forward(self, output, target, diff, phi, penalty):
         return torch.mean((target - output) ** 2 / len_partition + diff ** 2
-                          + (1 / penalty) ** 2 * func.relu(phi - output) ** 2)
+                          + (1 / penalty) ** 2 * func.relu(output - phi) ** 2)
 
 
 def main(penalty=0.01):
     model = Net()
     for i in tqdm(range(1, num_partition + 1)):
         input_data = torch.tensor(np.random.normal(loc=0, scale=sigma ** 2 / (2 * alpha), size=num_data)
-                                  .reshape(num_data, 1), requires_grad=False, dtype=torch.float32)
+                                  .reshape(num_data, 1), requires_grad=False, dtype=torch.float32).to(device)
         if i == 0:
             train_data_loader = data_loader(input_data, payoffs_function(input_data))
         else:
+            model = model.to(device)
             train_data_loader = data_loader(input_data, model(input_data))
 
         model = Net()
@@ -114,7 +115,7 @@ def main(penalty=0.01):
                 optimizer.step()
 
     test_datas = torch.tensor(np.array([[j / 10] for j in range(-50, 50 + 1)]),
-                              requires_grad=False, dtype=torch.float32)
+                              requires_grad=False, dtype=torch.float32).to(device)
     initial_variables = test_datas.cpu().clone().detach().numpy().flatten().tolist()
     value = model(test_datas).cpu().clone().detach().numpy().flatten().tolist()
     return initial_variables, value, 'epsilon={}'.format(penalty)
